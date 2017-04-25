5bd3dc84fb43ed7923435726a31fa946de4ccacf
amingRo <894144279@qq.com>
2017-04-25 11:25:40 +0800

    结构初步搭建完成

--- app.js
+++ app.js
@@ -42,5 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,function () {
+    console.log("Server Start!");
+})
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '万达主题娱乐中央预定系统' });
 });
-
+router.get('/chooseArea.exe', function(req, res, next) {
+    res.render('chooseArea', { title: '万达主题娱乐中央预定系统' });
+});
+router.get('/chooseTicket.exe', function(req, res, next) {
+    res.render('chooseTicket', { title: '万达主题娱乐中央预定系统' });
+});
+router.get('/confirmPackage.exe', function(req, res, next) {
+    res.render('confirmPackage', { title: '万达主题娱乐中央预定系统' });
+});
+router.get('/paySuccess.exe', function(req, res, next) {
+    res.render('paySuccess', { title: '万达主题娱乐中央预定系统' });
+});
 module.exports = router;
