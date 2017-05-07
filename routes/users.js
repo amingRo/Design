9250a5d476bf775c0e87c808adc3cec324054729
amingRo <894144279@qq.com>
2017-05-07 19:58:46 +0800

    用户界面

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:username', function(req, res, next) {
-  res.send('user:' + req.params.username);
+  // res.send('user:' + req.params.username);
+  res.render('users', { title: '万达主题娱乐中央预定系统' });
 });
 
 module.exports = router;
