4dfbe206ebc6fb21de4eee5affe5d3bd93e62b16
amingRo <894144279@qq.com>
2017-05-14 13:54:33 +0800

    用户界面

---
+++ Settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by amingRo on 2017/5/13.
+ */
+module.exports = {
+    cookieSecret:"myDesign",
+    db:"Design",
+    host:"localhost"
+}
---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by amingRo on 2017/5/13.
+ */
+var settings = require('../Settings');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+
+module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT,{}));
